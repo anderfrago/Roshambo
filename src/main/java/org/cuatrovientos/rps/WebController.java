0c3710b23e77f9219ffc2b5e08de799012536b73
Ander F.L. <ander_frago@cuatrovientos.org>
2024-08-20 20:07:51 +0200

    request paramater choice

--- src/main/java/org/cuatrovientos/rps/WebController.java
+++ src/main/java/org/cuatrovientos/rps/WebController.java
@@ -1,13 +1,17 @@
 package org.cuatrovientos.rps;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebController {
 	
 	@GetMapping("/playgame")
-	public String playGame() {
+	public String playGame(//TODO Add request parameter choice String theChoice) {
+		
+		//TODO Add attribute to the model "yourChoice", theChoice
 		
 		return "results";		
 	}
